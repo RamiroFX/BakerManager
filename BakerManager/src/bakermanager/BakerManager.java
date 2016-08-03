a0cf534a8e50da1f0b818b373c9b5a6114f79280
RamiroFX <mr.ramiro@hotmail.com>
2016-08-03 00:53:50 -0400

    Se añadio version de prueba

--- BakerManager/src/bakermanager/BakerManager.java
+++ BakerManager/src/bakermanager/BakerManager.java
@@ -5,16 +5,21 @@
  */
 package bakermanager;
 
+import DB_manager.DB_manager;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Ramiro
  */
 public class BakerManager {
-    
+
     private static UIManager.LookAndFeelInfo apariencias[] = UIManager.getInstalledLookAndFeels();
 
     public static void main(String[] args) {
@@ -29,7 +34,27 @@ public class BakerManager {
         } catch (UnsupportedLookAndFeelException ex) {
             Logger.getLogger(BakerManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-        Inicio inicio = new Inicio();
-        inicio.mostrarLogin();
+        Date today = Calendar.getInstance().getTime();
+        Calendar calendario = Calendar.getInstance();
+        calendario.set(Calendar.HOUR_OF_DAY, 0);
+        calendario.set(Calendar.MINUTE, 0);
+        calendario.set(Calendar.SECOND, 0);
+        calendario.set(Calendar.DAY_OF_MONTH, 10);
+        calendario.set(Calendar.MONTH, 7);//AGOSTO
+        calendario.set(Calendar.YEAR, 2016);
+        if (today.before(calendario.getTime())) {
+            Calendar temp = Calendar.getInstance();
+            int diaActual = temp.get(Calendar.DAY_OF_MONTH);
+            int diaLimite = calendario.get(Calendar.DAY_OF_MONTH);
+            int diferencia = diaLimite - diaActual;
+            String mensaje = "Le quedan " + diferencia + " día/s de prueba.";
+            JOptionPane.showMessageDialog(null, mensaje, "Baker Manager", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println(mensaje);
+            Inicio inicio = new Inicio();
+            inicio.mostrarLogin();
+        } else {
+            JOptionPane.showMessageDialog(null, "Ah expirado su tiempo de prueba. Contacte con soporte tecnico", "Baker Manager", JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        }
     }
 }
