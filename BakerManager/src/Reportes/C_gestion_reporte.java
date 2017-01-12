3101f7d6cd13c3614929827260004c3dee2c4e95
RamiroFX <mr.ramiro@hotmail.com>
2017-01-12 19:49:17 -0400

    Working on reports

--- BakerManager/src/Reportes/C_gestion_reporte.java
+++ BakerManager/src/Reportes/C_gestion_reporte.java
@@ -5,10 +5,42 @@
  */
 package Reportes;
 
+import bakermanager.C_inicio;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author Ramiro
  */
-public class C_gestion_reporte {
-    
+public class C_gestion_reporte implements ActionListener {
+
+    private V_gestion_reporte vista;
+    public C_inicio c_inicio;
+
+    public C_gestion_reporte(V_gestion_reporte vista, C_inicio c_inicio) {
+        this.vista = vista;
+        this.c_inicio = c_inicio;
+        concederPermisos();
+    }
+
+    public void mostrarVista() {
+        this.vista.setLocation(this.c_inicio.centrarPantalla(this.vista));
+        this.c_inicio.agregarVentana(this.vista);
+    }
+
+    private void concederPermisos() {
+        this.vista.jbRCD.addActionListener(this);
+        this.vista.jbRCS.addActionListener(this);
+        this.vista.jbRCSC.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src.equals(this.vista.jbRCSC)) {
+            FiltroReporte_Prov p = new FiltroReporte_Prov(c_inicio);
+            p.setVisible(true);
+        }
+    }
 }
---
+++ BakerManager/src/Reportes/FiltroReporte_Prov.java
@@ -0,0 +1,183 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reportes;
+
+import DB.DB_Producto;
+import DB.DB_Proveedor;
+import DB.DB_manager;
+import bakermanager.C_inicio;
+import com.toedter.calendar.JDateChooser;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.swing.JRViewer;
+
+/**
+ *
+ * @author Ramiro Ferreira
+ */
+public class FiltroReporte_Prov extends JDialog implements ActionListener {
+
+    JPanel jpProveedor, jpFecha, jpSouth;
+    JDateChooser jdcFechaInicio, jdcFechaFin;
+    JTable jtProdCategorias;
+    JButton jbQuitar, jbAgregar, jbGenerar, jbCancelar;
+
+    public FiltroReporte_Prov(C_inicio c_inicio) {
+        super(c_inicio.vista, "Filtro", false);
+        setSize(400, 600);
+        setLocationRelativeTo(c_inicio.vista);
+        inicializarVista();
+        concederPermisos();
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(jpFecha, BorderLayout.NORTH);
+        getContentPane().add(jpProveedor, BorderLayout.CENTER);
+        getContentPane().add(jpSouth, BorderLayout.SOUTH);
+        completarCampos();
+    }
+
+    private void inicializarVista() {
+        /*
+         * RANGO DE FECHAS
+         */
+        this.jdcFechaInicio = new JDateChooser();
+        this.jdcFechaFin = new JDateChooser();
+        this.jdcFechaFin.setDate(Calendar.getInstance().getTime());
+        this.jpFecha = new JPanel();
+        this.jpFecha.setBorder(javax.swing.BorderFactory.createTitledBorder("Rango de fechas"));
+        this.jpFecha.add(jdcFechaInicio);
+        this.jpFecha.add(jdcFechaFin);
+        /*
+         * PROVEEDORES
+         */
+        this.jtProdCategorias = new JTable();
+        JScrollPane jspProveedor = new JScrollPane(this.jtProdCategorias);
+        this.jbAgregar = new JButton("Agregar");
+        this.jbQuitar = new JButton("Quitar");
+        JPanel jpBotones = new JPanel();
+        jpBotones.add(jbAgregar);
+        jpBotones.add(jbQuitar);
+        this.jpProveedor = new JPanel();
+        this.jpProveedor.setLayout(new BorderLayout());
+        this.jpProveedor.setBorder(javax.swing.BorderFactory.createTitledBorder("Categorías"));
+        this.jpProveedor.add(jspProveedor, BorderLayout.CENTER);
+        this.jpProveedor.add(jpBotones, BorderLayout.SOUTH);
+        /*
+         * BOTONES
+         */
+        this.jbGenerar = new JButton("Generar");
+        this.jbCancelar = new JButton("Cancelar");
+        this.jpSouth = new JPanel();
+        this.jpSouth.add(jbCancelar);
+        this.jpSouth.add(jbGenerar);
+    }
+
+    private void completarCampos() {
+        this.jtProdCategorias.setModel(DB_manager.consultarCategoria());
+    }
+
+    private void concederPermisos() {
+        this.jbGenerar.addActionListener(this);
+        this.jbCancelar.addActionListener(this);
+        this.jbAgregar.addActionListener(this);
+        this.jbQuitar.addActionListener(this);
+    }
+
+    private void generarReporte() {
+        if (validarFechas(jdcFechaInicio.getDate(), jdcFechaFin.getDate())) {
+            System.out.println("sDate: " + jdcFechaInicio.getDate());
+            System.out.println("eDate: " + jdcFechaFin.getDate());
+            int row = this.jtProdCategorias.getModel().getRowCount();
+            ArrayList categories = new ArrayList();
+            for (int i = 0; i < row; i++) {
+                categories.add(Integer.valueOf(String.valueOf(this.jtProdCategorias.getModel().getValueAt(i, 0))));
+            }
+            File file = new File(System.getProperty("user.dir") + "\\src\\Assets\\Reportes\\ResumenComprasSimpleCategoria.jasper");
+            JasperReport reporte = null;
+            try {
+                reporte = (JasperReport) JRLoader.loadObject(file);
+            } catch (JRException ex) {
+                JOptionPane.showMessageDialog(this, "No se encontró la ubicación del reporte", "Atención", JOptionPane.WARNING_MESSAGE);
+            }
+
+            Calendar calendarStart = Calendar.getInstance();
+            calendarStart.setTime(jdcFechaInicio.getDate());
+            calendarStart.set(Calendar.HOUR_OF_DAY, 0);
+            calendarStart.set(Calendar.MINUTE, 0);
+            calendarStart.set(Calendar.SECOND, 0);
+            calendarStart.set(Calendar.MILLISECOND, 0);
+
+            Calendar calendarEnd = Calendar.getInstance();
+            calendarEnd.setTime(jdcFechaFin.getDate());
+            calendarEnd.set(Calendar.HOUR_OF_DAY, 23);
+            calendarEnd.set(Calendar.MINUTE, 59);
+            calendarEnd.set(Calendar.SECOND, 59);
+            calendarEnd.set(Calendar.MILLISECOND, 250);
+            try {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("sDate", new Timestamp(calendarStart.getTime().getTime()));
+                map.put("eDate", new Timestamp(calendarEnd.getTime().getTime()));
+                map.put("categorias", categories);
+                JasperPrint jp = JasperFillManager.fillReport(reporte, map, DB_manager.getConection());
+                JRViewer jv = new JRViewer(jp);
+                JFrame jf = new JFrame();
+                jf.getContentPane().add(jv);
+                jf.validate();
+                jf.setVisible(true);
+                jf.setSize(new Dimension(800, 600));
+                jf.setLocation(300, 100);
+                jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            } catch (JRException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            jdcFechaFin.setDate(this.jdcFechaInicio.getDate());
+            jdcFechaFin.updateUI();
+            JOptionPane.showMessageDialog(this, "La fecha inicio debe ser menor que fecha final", "Atención", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    boolean validarFechas(Date f_inicio, Date f_final) {
+        if (f_inicio != null && f_final != null) {
+            int dateValue = f_inicio.compareTo(f_final);
+            if (dateValue <= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src.equals(this.jbGenerar)) {
+            generarReporte();
+        }
+    }
+}
